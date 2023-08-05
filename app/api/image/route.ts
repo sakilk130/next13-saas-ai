8d8bfcaad5587a2a4cd38469c59a7fe13da8c3fd
Sakil Khan <sakilk130@gmail.com>
2023-08-05 22:55:40 +0600

    feat: image generate api

---
+++ app/api/image/route.ts
@@ -0,0 +1,35 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount = 1, resolution = '512x512' } = body;
+
+    if (!userId) new NextResponse('Unauthorized', { status: 401 });
+    if (!configuration.apiKey)
+      new NextResponse('OpenAI API Key not configured.', { status: 500 });
+    if (!prompt) new NextResponse('Prompt is required', { status: 400 });
+    if (!amount) new NextResponse('Amount is required', { status: 400 });
+    if (!resolution)
+      new NextResponse('Resolution is required', { status: 400 });
+
+    const response = await openai.createImage({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
+    });
+
+    return NextResponse.json(response.data.data);
+  } catch (error) {
+    console.log('[CONVERSATION_ERROR]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
